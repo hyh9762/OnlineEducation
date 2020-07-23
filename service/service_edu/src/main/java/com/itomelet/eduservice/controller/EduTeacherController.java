e1fa2ca2864e66325619397998723bd09cddd451
hyh <21827014@zju.edu.cn>
2020-07-23 22:36:45 +0800

    整合swagger2

--- service/service_edu/src/main/java/com/itomelet/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/itomelet/eduservice/controller/EduTeacherController.java
@@ -3,6 +3,9 @@ package com.itomelet.eduservice.controller;
 
 import com.itomelet.eduservice.entity.EduTeacher;
 import com.itomelet.eduservice.service.EduTeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -16,11 +19,11 @@ import java.util.List;
  * @author ekko
  * @since 2020-07-23
  */
+@Api(tags = {"讲师管理"})
 @RestController
 @RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
 
-    //把service注入
     @Resource
     private EduTeacherService eduTeacherService;
 
@@ -30,13 +33,15 @@ public class EduTeacherController {
      * @return teacher集合
      */
     @GetMapping("/findAll")
+    @ApiOperation("讲师列表")
     public List<EduTeacher> findAlTeacher() {
         return eduTeacherService.list(null);
     }
 
 
+    @ApiOperation("根据id删除讲师")
     @DeleteMapping("/{id}")
-    public boolean removeTeacher(@PathVariable String id) {
+    public boolean removeTeacher(@ApiParam("讲师id") @PathVariable String id) {
         return eduTeacherService.removeById(id);
     }
 }
