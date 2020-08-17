b610673b91598b77d4c0108f131218b4b29b7ffc
ekko <21827014@zju.edu.cn>
2020-08-17 16:23:24 +0800

    前端整合阿里云播放器，视频id获取视频凭证接口

--- service/service_edu/src/main/java/com/itomelet/eduservice/entity/vo/chapter/VideoVo.java
+++ service/service_edu/src/main/java/com/itomelet/eduservice/entity/vo/chapter/VideoVo.java
@@ -6,5 +6,6 @@ import lombok.Data;
 public class VideoVo {
     private String id;
     private String title;
+    private String videoSourceId;
 
 }
--- service/service_vod/src/main/java/com/itomelet/vod/controller/VodController.java
+++ service/service_vod/src/main/java/com/itomelet/vod/controller/VodController.java
@@ -1,7 +1,13 @@
 package com.itomelet.vod.controller;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthRequest;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthResponse;
 import com.itomelet.commonutils.Result;
+import com.itomelet.servicebase.exception.GuliException;
 import com.itomelet.vod.service.VodService;
+import com.itomelet.vod.utils.ConstantPropertiesUtils;
+import com.itomelet.vod.utils.InitVodClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -35,5 +41,25 @@ public class VodController {
     public Result deleteBatch(@RequestParam("videoIdList") List<String> videoList) {
         vodService.removeVideoList(videoList);
         return Result.success();
+    }
+
+    //根据视频id获取视频凭证
+    @GetMapping("/getPlayAuth/{id}")
+    public Result getPlayAuth(@PathVariable String id) {
+        try {
+            //创建初始化对象
+            DefaultAcsClient client =
+                    InitVodClient.initVodClient(ConstantPropertiesUtils.KEY_ID, ConstantPropertiesUtils.KEY_SECRET);
+            //创建获取凭证request和response对象
+            GetVideoPlayAuthRequest request = new GetVideoPlayAuthRequest();
+            //向request设置视频id
+            request.setVideoId(id);
+            //调用方法得到凭证
+            GetVideoPlayAuthResponse response = client.getAcsResponse(request);
+            String playAuth = response.getPlayAuth();
+            return Result.success().data("playAuth", playAuth);
+        } catch (Exception e) {
+            throw new GuliException(20001, "获取凭证失败");
+        }
     }
 }
