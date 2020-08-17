0772b577e4eec5305a6447531b0d278d3aacea52
hyh <21827014@zju.edu.cn>
2020-08-17 22:33:19 +0800

    根据id查询订单详情，订单状态

--- service/service_order/src/main/java/com/itomelet/order/controller/OrderController.java
+++ service/service_order/src/main/java/com/itomelet/order/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.itomelet.order.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.itomelet.commonutils.JwtUtils;
 import com.itomelet.commonutils.Result;
+import com.itomelet.order.entity.Order;
 import com.itomelet.order.service.OrderService;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,5 +34,28 @@ public class OrderController {
         String orderNo = orderService.createOrder(courseId, memberId);
         return Result.success().data("orderNo", orderNo);
     }
+
+    //2 根据订单id查询订单信息
+    @GetMapping("getOrderInfo/{orderNo}")
+    public Result getOrderInfo(@PathVariable String orderNo) {
+        QueryWrapper<Order> wrapper = new QueryWrapper<>();
+        wrapper.lambda().eq(Order::getOrderNo, orderNo);
+        Order order = orderService.getOne(wrapper);
+        return Result.success().data("item", order);
+    }
+
+
+    //根据课程id和用户id查询订单表中订单状态
+    @GetMapping("isBuyCourse/{courseId}/{memberId}")
+    public boolean isBuyCourse(@PathVariable String courseId, @PathVariable String memberId) {
+        QueryWrapper<Order> wrapper = new QueryWrapper<>();
+        wrapper.lambda().eq(Order::getCourseId, courseId);
+        wrapper.lambda().eq(Order::getMemberId, memberId);
+        wrapper.lambda().eq(Order::getStatus, 1);//支付状态 1代表已经支付
+        int count = orderService.count(wrapper);
+        //已经支付
+        return count > 0;
+    }
+
 }
 
