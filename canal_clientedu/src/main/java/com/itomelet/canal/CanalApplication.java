77a9dd1f901a42f7065f2f752e306358368eccc6
ekko <21827014@zju.edu.cn>
2020-08-19 10:59:32 +0800

    整合canal 数据库同步工具

---
+++ canal_clientedu/src/main/java/com/itomelet/canal/CanalApplication.java
@@ -0,0 +1,24 @@
+package com.itomelet.canal;
+
+import com.itomelet.canal.client.CanalClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+public class CanalApplication implements CommandLineRunner {
+
+    @Resource
+    private CanalClient canalClient;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class);
+    }
+
+    @Override
+    public void run(String... args) {
+        canalClient.run();
+    }
+}
